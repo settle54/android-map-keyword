018eb0e4cc85fb7863c783e8d48a24e315483cb7
settle54 <settle54@kangwon.ac.kr>
2024-07-02 15:19:07 +0900

    feat: make Place.kt and PlaceDBHelper.kt

---
+++ app/src/main/java/campus/tech/kakao/map/Place.kt
@@ -0,0 +1,7 @@
+package campus.tech.kakao.map
+
+data class Place (
+    val name: String,
+    val adress: String,
+    val category: String = ""
+)
---
+++ app/src/main/java/campus/tech/kakao/map/PlaceDBHelper.kt
@@ -0,0 +1,30 @@
+package campus.tech.kakao.map
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class PlaceDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "PlaceTable.db"
+        private const val DATABASE_VERSION = 1
+
+        private const val TABLE_NAME = "PlaceTable"
+        private const val NAME = "NAME"
+        private const val ADDRESS = "ADDRESS"
+        private const val CATEGORY = "CATEGORY"
+
+        private const val CREATE_TABLE = "CREATE TABLE IF NOT EXISTS $TABLE_NAME (" +
+                "$NAME text, $ADDRESS text, $CATEGORY text)"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+}
